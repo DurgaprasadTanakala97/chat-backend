b7b0b9973f310b0c0d255cc220c462ee3270a5d7
Durga Prasad <durgaprasadtanakala97@gmail.com>
2026-04-18 16:46:41 +0530

    added remote mongo url

--- database/db.js
+++ database/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
-export const dbConnection = ()=>{
-    mongoose.connect(process.env.MONGO_URL,{
-        dbName:'QUICK_CHAT_BACKEND'
+export const dbConnection = async ()=>{
+    console.log(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI,{
+        dbName:'QUICK_CHAT_BACKEND',
+        family: 4, // forces IPv4
+        serverSelectionTimeoutMS: 5000
     }).then(()=>{
         console.log('mongodatabase connected successfully')
     }).catch((err)=>{
